1b5d489e056714be3e1b8c6eab44c7d9d7792902
Thales Pereira Tenebra <thalestenebra@outlook.com>
2021-04-07 01:35:45 -0300

    Creating a Custom Dynamic Input Component

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -2,39 +2,44 @@ import React, { useEffect } from "react";
 import classes from "./Cockpit.css";
 
 const cockpit = (props) => {
-  useEffect(() => {
-    console.log("[Cockpit.js] useEffect");
-    // http request...
-    // Request example
-    setTimeout(() => {
-      alert("Saved data to cloud!");
-    }, 1000);
-    // Array down trigger what will make useEffect works, void one triggers once
-  }, []);
+    useEffect(() => {
+        console.log("[Cockpit.js] useEffect");
+        // http request...
+        // Request example
+        const timer = setTimeout(() => {
+            //alert("Saved data to cloud!");
+        }, 1000);
+        return () => {
+            clearTimeout(timer);
+            console.log("[Cockpit.js] cleanup work in useEffect");
+        };
 
-  // useEffect(() => {}) you can use whateveryou want
+        // Array down trigger what will make useEffect works, void one triggers once
+    }, []);
 
-  const classesAssigned = [];
-  let btnClass = "";
-  if (props.persons.length <= 2) {
-    classesAssigned.push(classes.red);
-  }
-  if (props.persons.length <= 1) {
-    classesAssigned.push(classes.bold);
-  }
-  if (props.showPersons) {
-    btnClass = classes.Red;
-  }
-  console.log("[Cockpit.js] Component rendering...");
-  return (
-    <div className={classes.Cockpit}>
-      <h1>{props.title}</h1>
-      <p className={classesAssigned.join(" ")}>This is really working!</p>
-      <button className={btnClass} onClick={props.clicked}>
-        Toggle Persons
-      </button>
-    </div>
-  );
+    // useEffect(() => {}) you can use whateveryou want
+
+    const classesAssigned = [];
+    let btnClass = "";
+    if (props.persons.length <= 2) {
+        classesAssigned.push(classes.red);
+    }
+    if (props.persons.length <= 1) {
+        classesAssigned.push(classes.bold);
+    }
+    if (props.showPersons) {
+        btnClass = classes.Red;
+    }
+    console.log("[Cockpit.js] Component rendering...");
+    return (
+        <div className={classes.Cockpit}>
+            <h1>{props.title}</h1>
+            <p className={classesAssigned.join(" ")}>This is really working!</p>
+            <button className={btnClass} onClick={props.clicked}>
+                Toggle Persons
+            </button>
+        </div>
+    );
 };
 
 export default cockpit;
